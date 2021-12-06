a5f7054b8ff050ecf5dfe23372d4eb807d737aa6
NGuyen Duc <91659267+zxmclzxmcl@users.noreply.github.com>
2021-12-06 22:23:44 +0700

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,23 @@
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+ 
+public class App extends Application 
+{
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Tableview.fxml"));
+        Scene scene = new Scene(root);
+        
+        stage.setTitle("Staff Management Program");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
